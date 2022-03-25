5ab954f54b7b9edb7be352c077422ee0788eb0b1
Ciraudi <66919459+Ciraudi@users.noreply.github.com>
2022-03-25 23:23:28 +0100

    Update triples_extraction.py

--- triples_extraction.py
+++ triples_extraction.py
@@ -1,3 +1,29 @@
+/* 
+ * 
+ * Group:
+ * Ciraudo	Egidio      0622701566  e.ciraudo@studenti.unisa.it           
+ * Aleksandruk Lyubov   0622701472	l.aleksandruk@studenti.unisa.it 
+ * Valentino Daniele    0622701069  d.valentino5@studenti.unisa.it
+ * D'Amato Giuseppina   0622701577  g.damato43@studenti.unisa.it
+ *
+ * Copyright (C) 2022 - All Rights Reserved 
+ *
+ * This file is part of semantic-extraction.
+ *
+ * semantic-extraction is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semantic-extraction is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with semantic-extraction.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 from openie import StanfordOpenIE
 import nltk
 from nltk.stem import WordNetLemmatizer
